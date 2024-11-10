c7fd59aa6cb3634ef1fedb7e59953284885e3018
Inkwizzz <juromskij.vlad@mail.ru>
2024-11-10 21:23:08 +0300

    Finished release

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jurom/CLionProjects/untitled1
+# Build directory: C:/Users/jurom/CLionProjects/untitled1/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/jurom/CLionProjects/untitled1/CMakeLists.txt;86;add_test;C:/Users/jurom/CLionProjects/untitled1/CMakeLists.txt;0;")
